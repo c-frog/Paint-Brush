6d755cd1e8f2ef6d1a9f342a0f3d4f69916a74c4
tzvik15 <tzvik15@yahoo.com>
2019-12-17 16:01:18 -0700

    successful proof of concept:capturing mouse coordinates

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,6 @@
+const testArr=[];
+
+
 
  function setup() {
   let cnv = createCanvas(640, 480);
@@ -9,8 +12,21 @@
 
 function draw() {
   if (mouseIsPressed) {
+   test();
       fill(0);
     line(mouseX, mouseY, pmouseX, pmouseY);
   } 
   
+}
+
+
+$("#test").on("click", function(){
+  const uniqueSet = new Set(testArr);
+  const backToArr=[...uniqueSet];
+  console.log(backToArr);
+})
+
+
+const test=()=>{
+  testArr.push(mouseX + ":" +mouseY);
 }
