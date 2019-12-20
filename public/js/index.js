22791cbb71954f8b5545380a1c51009433dfc924
tzvik15 <tzvik15@yahoo.com>
2019-12-20 11:05:59 -0700

    solved corrupted coordinate retrival, created a redraw function that makes an image from coordinates

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,48 @@
-const testArr=[];
+const testArr = [];
+const xArr = [126, 129, 156, 134];
+const yArr = [45, 28, 124, 400];
 
-
-
- function setup() {
+function setup() {
   let cnv = createCanvas(640, 480);
-  let x = (windowWidth - width) /2;
-  let y = (windowHeight - height) /2;
-  cnv.position(x,y);
-  
+  let x = (windowWidth - width) / 2;
+  let y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  $("#defaultCanvas0")
+    .mousedown(function() {
+      timer = setInterval(test, 100);
+    })
+    .mouseup(function() {
+      clearInterval(timer);
+    });
 }
 
 function draw() {
   if (mouseIsPressed) {
-   test();
-      fill(0);
     line(mouseX, mouseY, pmouseX, pmouseY);
-  } 
-  
+  }
 }
 
+$("#go").on("click", function() {
+  var c = document.getElementById("defaultCanvas0");
+  var ctx = c.getContext("2d");
 
-$("#test").on("click", function(){
-  const uniqueSet = new Set(testArr);
-  const backToArr=[...uniqueSet];
-  console.log(backToArr);
-})
+  ctx.beginPath();
+  ctx.moveTo(testArr[0], testArr[1]);
+  for (let i = 2; i < testArr.length; i += 2) {
+    if (typeof testArr[i + 3] === "number") {
+      ctx.lineTo(testArr[i], testArr[i + 1]);
+    }
+  }
+  ctx.stroke();
+});
 
+$("#clean").on("click", function() {
+  clear();
+});
+$("#printArr").on("click", function() {
+  console.log(testArr);
+});
 
-const test=()=>{
-  testArr.push(mouseX + ":" +mouseY);
-}
+const test = () => {
+  testArr.push(mouseX, mouseY);
+};
