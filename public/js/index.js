70ea03d36618822b0c59e3153780c2facd60efbf
tzvik15 <tzvik15@yahoo.com>
2020-01-11 08:28:48 -0700

    debugging

--- public/js/index.js
+++ public/js/index.js
@@ -196,20 +196,20 @@ $("#modalOpen").on("click", function(ev) {
    // var modal = document.getElementById("myModal");
     
     // Get the button that opens the modal
-    var btn = document.getElementById("modalOpen");
+    //var btn = document.getElementById("modalOpen");
     
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    //var span = document.getElementsByClassName("close")[0];
     
     // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
+    // btn.onclick = function() {
+    //   modal.style.display = "block";
+    // }
     
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
+    // // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() {
+    //   modal.style.display = "none";
+    // }
     
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
@@ -220,36 +220,36 @@ $("#modalOpen").on("click", function(ev) {
     
     
     
-    $("#modalOpen").on("click", function(ev) {
-      ev.preventDefault();
-      $.ajax({
-        method: "GET",
-        url:"/api/images"
-      }).then(function(res) {
-        $("#loadBtns").html("");
-        for (let i = 0; i< res.length; i++) {
-          let loadBtn = $("<button>");
-          let dataCoord = JSON.parse(res[i].coordinates);
+    // $("#modalOpen").on("click", function(ev) {
+    //   ev.preventDefault();
+    //   $.ajax({
+    //     method: "GET",
+    //     url:"/api/images"
+    //   }).then(function(res) {
+    //     $("#loadBtns").html("");
+    //     for (let i = 0; i< res.length; i++) {
+    //       let loadBtn = $("<button>");
+    //       let dataCoord = JSON.parse(res[i].coordinates);
           
-          loadBtn.text(res[i].name);
-          loadBtn.attr("class", "toolBtn retBtn");
-          loadBtn.attr("data-coord", dataCoord.array);
-          loadBtn.attr("data-color", res[i].color);
-          $("#loadBtns").append(loadBtn);
-        }
-        $(".retBtn").on("click", function(){
-          event.stopPropagation()
-          testArr = [];
-          colorChoice = event.target.attributes[2].nodeValue;
-          let returnData = event.target.attributes[1].nodeValue
-          let newArrOne = returnData.split(",");
-          for (let j=0; j<newArrOne.length; j++) {
-            testArr.push(parseFloat(newArrOne[j]));
-          }
-        })
-      })
+    //       loadBtn.text(res[i].name);
+    //       loadBtn.attr("class", "toolBtn retBtn");
+    //       loadBtn.attr("data-coord", dataCoord.array);
+    //       loadBtn.attr("data-color", res[i].color);
+    //       $("#loadBtns").append(loadBtn);
+    //     }
+    //     $(".retBtn").on("click", function(){
+    //       event.stopPropagation()
+    //       testArr = [];
+    //       colorChoice = event.target.attributes[2].nodeValue;
+    //       let returnData = event.target.attributes[1].nodeValue
+    //       let newArrOne = returnData.split(",");
+    //       for (let j=0; j<newArrOne.length; j++) {
+    //         testArr.push(parseFloat(newArrOne[j]));
+    //       }
+    //     })
+    //   })
 
-    })
+    // })
 
     $("#testBack").on("click", function(event) {
       event.preventDefault();
